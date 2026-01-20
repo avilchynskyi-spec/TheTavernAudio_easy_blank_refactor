ea2e50132d4c4206ab13b2be693af35c62aaf69d
Andrii Vilchynskyi <avilchynskyi@edu.cdv.pl>
2026-01-20 17:59:55 +0100

    FIreplace_Interact (on/off)

---
+++ Assets/Scripts/Fireplace_Interact.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using FMODUnity;
+
+public class Fireplace_Interact : MonoBehaviour, IInteractable 
+{
+    [Header(("Ognisko"))]
+    [SerializeField] GameObject ognisko;
+    [Header("Dzwieki")]
+    [SerializeField] private EventReference fireplaceStart;
+    [SerializeField] private EventReference fireplaceStop;
+    [Header("Stan")]
+    [SerializeField] private bool isActive = true;
+   public void Interact ()
+    {
+        isActive = !isActive;
+        if (ognisko != null)
+        {
+            ognisko.SetActive(isActive);
+            PlayInterctSound();
+        }
+
+
+    }
+
+    public void PlayInterctSound()
+
+    {
+        if (isActive)
+        {
+            RuntimeManager.PlayOneShot(fireplaceStart);
+        }
+        else
+        {
+            RuntimeManager.PlayOneShot(fireplaceStop);
+        }
+
+    }
+
+
+}
+
+
